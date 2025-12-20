379854c367a59c8ce42e5a479042c5bbd62afe26
moon-lightTT <winter-forestt@mail.ru>
2025-12-20 14:43:07 +0300

    workflow and deploy

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
     <div class="logo">
-      <img :src="logoType === 'alternative' ? '/logoAlt.svg' : '/logo.svg'" 
+      <img :src="logoType === 'alternative' ? 'logoAlt.svg' : 'logo.svg'" 
            alt="logo" 
            class="logo-img">
       <h1 class="text-logo">KidsConnect</h1>
