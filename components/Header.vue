766100d15f3e0dc3cc8da505a439bbdde322ab23
moon-lightTT <winter-forestt@mail.ru>
2025-12-20 16:29:22 +0300

    check

--- components/Header.vue
+++ components/Header.vue
@@ -6,20 +6,3 @@
     </div>
   </header>
 </template>
-
-<style scoped>
-.logo-img {
-  width: 50px;
-  height: 50px;
-  background-size: contain;
-  background-repeat: no-repeat;
-}
-
-.logo-img.default {
-  background-image: url('/logo.svg');
-}
-
-.logo-img.alternative {
-  background-image: url('/logoAlt.svg');
-}
-</style>
