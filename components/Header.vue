06980d7630c9e5e1567ac91a8b3d642bf7bb0a0a
moon-lightTT <winter-forestt@mail.ru>
2025-12-20 15:12:58 +0300

    workflow and deploy

--- components/Header.vue
+++ components/Header.vue
@@ -1,22 +1,14 @@
 <template>
   <header class="header">
     <div class="logo">
-      <img :src="currentLogo" 
-           alt="logo" 
-           class="logo-img">
+      <NuxtImg 
+        :src="logoType === 'alternative' ? '/logoAlt.svg' : '/logo.svg'" 
+        alt="logo" 
+        class="logo-img"
+        width="50"
+        height="50"
+      />
       <h1 class="text-logo">KidsConnect</h1>
     </div>
   </header>
 </template>
-
-<script setup>
-import logoDefault from '~/public/logo.svg'
-import logoAlt from '~/public/logoAlt.svg'
-
-const props = defineProps({
-  logoType: { type: String, default: 'default' }
-})
-const currentLogo = computed(() => {
-  return props.logoType === 'alternative' ? logoAlt : logoDefault
-})
-</script>
